adac5b024a785dd1b9f33a6c9dd9346fdac91401
Dan1eLLa1 <ilsiyar_salahova@mail.ru>
2024-12-20 23:19:15 +0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Rc5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonEncrypt = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.txtKey = new System.Windows.Forms.TextBox();
+            this.buttonDecrypt = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonEncrypt
+            // 
+            this.buttonEncrypt.Location = new System.Drawing.Point(88, 320);
+            this.buttonEncrypt.Name = "buttonEncrypt";
+            this.buttonEncrypt.Size = new System.Drawing.Size(95, 20);
+            this.buttonEncrypt.TabIndex = 0;
+            this.buttonEncrypt.Text = "Зашифровать";
+            this.buttonEncrypt.UseVisualStyleBackColor = true;
+            this.buttonEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(65, 62);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(136, 252);
+            this.txtInput.TabIndex = 1;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(349, 62);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(136, 252);
+            this.txtOutput.TabIndex = 2;
+            // 
+            // txtKey
+            // 
+            this.txtKey.Location = new System.Drawing.Point(222, 320);
+            this.txtKey.Name = "txtKey";
+            this.txtKey.Size = new System.Drawing.Size(106, 20);
+            this.txtKey.TabIndex = 3;
+            // 
+            // buttonDecrypt
+            // 
+            this.buttonDecrypt.Location = new System.Drawing.Point(365, 319);
+            this.buttonDecrypt.Name = "buttonDecrypt";
+            this.buttonDecrypt.Size = new System.Drawing.Size(95, 20);
+            this.buttonDecrypt.TabIndex = 4;
+            this.buttonDecrypt.Text = "Расшифровать";
+            this.buttonDecrypt.UseVisualStyleBackColor = true;
+            this.buttonDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(259, 295);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Ключ";
+            this.label1.UseCompatibleTextRendering = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(85, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Ввести текст";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(386, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Результат";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonDecrypt);
+            this.Controls.Add(this.txtKey);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.buttonEncrypt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonEncrypt;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.TextBox txtKey;
+        private System.Windows.Forms.Button buttonDecrypt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Rc5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+            private void btnEncrypt_Click(object sender, EventArgs e)
+            {
+                string plainText = txtInput.Text;
+                string key = txtKey.Text;
+
+                try
+                {
+                    RC5 rc5 = new RC5(key);
+                    string encryptedText = rc5.Encrypt(plainText);
+                    txtOutput.Text = encryptedText;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+            private void btnDecrypt_Click(object sender, EventArgs e)
+            {
+                string encryptedText = txtInput.Text;
+                string key = txtKey.Text;
+
+                try
+                {
+                    RC5 rc5 = new RC5(key);
+                    string decryptedText = rc5.Decrypt(encryptedText);
+                    txtOutput.Text = decryptedText;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+    }
+
+    public class RC5
+        {
+            private const int WordSize = 64;
+            private const int Rounds = 12;
+            private const int KeyLength = 16;
+            private uint[] S;
+
+            public RC5(string key)
+            {
+                InitializeKey(Encoding.UTF8.GetBytes(key));
+            }
+
+            private void InitializeKey(byte[] key)
+            {
+                int words = (KeyLength + 3) / 4;
+                uint[] L = new uint[words];
+
+                for (int i = 0; i < key.Length; i++)
+                {
+                    L[i / 4] = (L[i / 4] << 8) + key[i];
+                }
+
+                S = new uint[2 * (Rounds + 1)];
+                S[0] = 0xB7E15163;
+                for (int i = 1; i < S.Length; i++)
+                {
+                    S[i] = S[i - 1] + 0x9E3779B9;
+                }
+
+                uint A = 0, B = 0;
+                int iter = 3 * Math.Max(words, S.Length);
+
+                for (int k = 0, i = 0, j = 0; k < iter; k++)
+                {
+                    A = S[i] = RotateLeft(S[i] + A + B, 3);
+                    B = L[j] = RotateLeft(L[j] + A + B, (int)(A + B));
+                    i = (i + 1) % S.Length;
+                    j = (j + 1) % words;
+                }
+            }
+
+            public string Encrypt(string plainText)
+            {
+                byte[] data = Encoding.UTF8.GetBytes(plainText);
+                int paddedLength = (data.Length + 7) / 8 * 8;
+                byte[] paddedData = new byte[paddedLength];
+                Array.Copy(data, paddedData, data.Length);
+
+                StringBuilder result = new StringBuilder();
+
+                for (int i = 0; i < paddedData.Length; i += 8)
+                {
+                    uint A = BitConverter.ToUInt32(paddedData, i);
+                    uint B = BitConverter.ToUInt32(paddedData, i + 4);
+
+                    A += S[0];
+                    B += S[1];
+
+                    for (int round = 1; round <= Rounds; round++)
+                    {
+                        A = RotateLeft(A ^ B, (int)B) + S[2 * round];
+                        B = RotateLeft(B ^ A, (int)A) + S[2 * round + 1];
+                    }
+
+                    result.Append(BitConverter.ToString(BitConverter.GetBytes(A)));
+                    result.Append(BitConverter.ToString(BitConverter.GetBytes(B)));
+                }
+
+                return result.ToString().Replace("-", "");
+            }
+
+            public string Decrypt(string cipherText)
+            {
+                byte[] data = new byte[cipherText.Length / 2];
+                for (int i = 0; i < data.Length; i++)
+                {
+                    data[i] = Convert.ToByte(cipherText.Substring(i * 2, 2), 16);
+                }
+
+                StringBuilder result = new StringBuilder();
+
+                for (int i = 0; i < data.Length; i += 8)
+                {
+                    uint A = BitConverter.ToUInt32(data, i);
+                    uint B = BitConverter.ToUInt32(data, i + 4);
+
+                for (int round = Rounds; round > 0; round--)
+                    {
+                        B = RotateRight(B - S[2 * round + 1], (int)A) ^ A;
+                        A = RotateRight(A - S[2 * round], (int)B) ^ B;
+                    }
+
+                    B -= S[1];
+                    A -= S[0];
+
+                    result.Append(Encoding.UTF8.GetString(BitConverter.GetBytes(A)));
+                    result.Append(Encoding.UTF8.GetString(BitConverter.GetBytes(B)));
+                }
+
+                return result.ToString().TrimEnd('\0');
+            }
+
+            private uint RotateLeft(uint value, int shift)
+            {
+                return (value << shift) | (value >> (WordSize - shift));
+            }
+
+            private uint RotateRight(uint value, int shift)
+            {
+                return (value >> shift) | (value << (WordSize - shift));
+            }
+        }
+}
+
+
